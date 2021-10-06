0f0d397520157f3d272aae7d8d8b55d13f12406b
Deepak <71409090+deepak-sdk@users.noreply.github.com>
2021-10-06 14:39:18 +0530

    country-card-static

---
+++ script.js
@@ -0,0 +1,12 @@
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://restcountries.com/v3.1/all", true);
+// xhr.responseType = "json";
+// xhr.onload = () => {
+//   const allcountriesflags = xhr.response;
+//   console.log(allcountriesflags);
+//   var Population = allcountriesflags.filter(
+//     (country) => country.population < 200000
+//   );
+
+// };
+// xhr.send();
